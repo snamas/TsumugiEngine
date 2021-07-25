cd0217ac0b9f74010268fb480c26bb8923a1e6ba
Snamas <43674314+KominamiYuto@users.noreply.github.com>
2021-07-25 15:58:07 +0900

    メモリの二重解放を解決した。 あとpinを使うことで安全にメモリアドレスを固定できるようになった？

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,105 @@
 use std::any::{TypeId, Any};
-use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc::{Sender, Receiver, Iter};
 use std::sync::mpsc;
 use std::task::Poll;
+use std::thread;
+use std::thread::JoinHandle;
+use std::pin::Pin;
 
-trait TsumugiFuture:TsumugiTypeChacher{
+pub trait TsumugiFuture: TsumugiTypeChacher {
     fn poll(self: &mut Self) -> Poll<()>;
-    fn input_item(self: &mut Self, input_item: Box<dyn TsumugiTypeChacher+ Send>);
+    fn input_item(self: &mut Self, input_item: Box<dyn TsumugiTypeChacher + Send>);
 }
-trait TsumugiTypeChacher{
+
+pub trait TsumugiTypeChacher {
     fn as_any(&mut self) -> &mut dyn Any;
-    fn typehash(&self)->TypeId;
+    fn typehash(&self) -> TypeId;
 }
-trait TsumugiObject{
+
+trait TsumugiObject {
     fn on_create(&self);
 }
+
 pub struct TsumugiController {
-    pickup_list:Vec<Box<dyn TsumugiTypeChacher+ Send>>,
-    pickup_channnel_receiver:Receiver<Box<dyn TsumugiTypeChacher+ Send>>,
-    pickup_channnel_sender:Sender<Box<dyn TsumugiTypeChacher+ Send>>,
-    receipt_list:Vec<Box<dyn TsumugiFuture+ Send>>,
-    receipt_channnel_receiver:Receiver<Box<dyn TsumugiFuture+ Send>>,
-    receipt_channnel_sender:Sender<Box<dyn TsumugiFuture+ Send>>,
-    global_tsumugi_controller:Option<&'static Box<TsumugiController>>,
-    connect_tsumugi_controller:Vec<String>,
-    global_connect_tsumugi_controller:Vec<Box<TsumugiController>>,
-    tsumugi_controller_name:String,
-    tsumugi_object_vector:Vec<Box<dyn TsumugiObject+ Send>>
+    pickup_list: Vec<Box<dyn TsumugiTypeChacher + Send>>,
+    pub pickup_channnel_sender: Sender<Box<dyn TsumugiTypeChacher + Send>>,
+    receipt_list: Vec<Box<dyn TsumugiFuture + Send>>,
+    pub receipt_channnel_sender: Sender<Box<dyn TsumugiFuture + Send >>,
+    global_tsumugi_controller: Option<*const Pin<Box<TsumugiController>>>,
+    connect_tsumugi_controller: Vec<String>,
+    global_connect_tsumugi_controller: Vec<Box<TsumugiController>>,
+    tsumugi_controller_name: String,
+    tsumugi_object_vector: Vec<Box<dyn TsumugiObject + Send>>,
 }
-trait TsumugiControllerTrait {
-    fn new(tsumuginame:String)->TsumugiController;
-    fn spown(&self,tsumuginame:String)->Box<TsumugiController> ;
-    fn set_object(&mut self, tsumugi_object_list: Vec<Box<dyn TsumugiObject+ Send>>);
-    fn run(& mut self, tsumugi_functions:& dyn Fn(& TsumugiController) -> Box<TsumugiController>);
+
+pub trait TsumugiControllerTrait {
+    fn new(tsumuginame: String) -> Pin<Box<TsumugiController>>;
+    fn spown(&self, tsumuginame: String) -> Pin<Box<TsumugiController>>;
+    fn set_object(&mut self, tsumugi_object_list: Vec<Box<dyn TsumugiObject + Send>>);
+    fn execute_tsumugi_functions(&mut self, tsumugi_functions: &dyn Fn(&TsumugiController) -> Box<TsumugiController>);
+    fn execute_tsumugi_thread(&self, receipt_channnel_receiver: Receiver<Box<dyn TsumugiFuture + Send>>, pickup_channnel_receiver: Receiver<Box<dyn TsumugiTypeChacher + Send>>) -> JoinHandle<()>;
 }
-impl  TsumugiControllerTrait  for TsumugiController {
-    fn new(tsumuginame:String) -> TsumugiController  {
-        let (mut txreceivebox, mut rxreceivebox): (Sender<Box<dyn TsumugiFuture+ Send>>, Receiver<Box<dyn TsumugiFuture+ Send>>) = mpsc::channel();
-        let (txsendbox, rxsendbox): (Sender<Box<dyn TsumugiTypeChacher+ Send>>, Receiver<Box<dyn TsumugiTypeChacher+ Send>>) = mpsc::channel();
-        let mut tumugi_receipt_list:Vec<Box<dyn TsumugiFuture+ Send>> = Vec::new();
-        let mut pickup_list: Vec<Box<dyn TsumugiTypeChacher+ Send>> =  Vec::new();
-        let mut tsumugi_connect_list: Vec<String> =  Vec::new();
-        let mut tsumugi_object_list: Vec<Box<dyn TsumugiObject+ Send>> =  Vec::new();
-        return TsumugiController {
+
+impl TsumugiControllerTrait for TsumugiController {
+    fn new(tsumuginame: String) -> Pin<Box<TsumugiController>> {
+        let (txreceivebox, rxreceivebox): (Sender<Box<dyn TsumugiFuture + Send>>, Receiver<Box<dyn TsumugiFuture + Send>>) = mpsc::channel();
+        let (txsendbox, rxsendbox): (Sender<Box<dyn TsumugiTypeChacher + Send>>, Receiver<Box<dyn TsumugiTypeChacher + Send>>) = mpsc::channel();
+        let mut tumugi_receipt_list: Vec<Box<dyn TsumugiFuture + Send>> = Vec::new();
+        let mut pickup_list: Vec<Box<dyn TsumugiTypeChacher + Send>> = Vec::new();
+        let mut tsumugi_connect_list: Vec<String> = Vec::new();
+        let mut tsumugi_object_list: Vec<Box<dyn TsumugiObject + Send>> = Vec::new();
+        let mut tc = Box::pin(TsumugiController {
             pickup_list: pickup_list,
-            pickup_channnel_receiver: rxsendbox,
             pickup_channnel_sender: txsendbox,
             receipt_list: tumugi_receipt_list,
-            receipt_channnel_receiver: rxreceivebox,
             receipt_channnel_sender: txreceivebox,
             global_tsumugi_controller: None,
             connect_tsumugi_controller: tsumugi_connect_list,
             global_connect_tsumugi_controller: vec![],
             tsumugi_controller_name: tsumuginame,
-            tsumugi_object_vector: tsumugi_object_list };
-    }
-
-    fn spown(&self,tsumuginame:String) -> Box<TsumugiController> {
-        let (mut txreceivebox, mut rxreceivebox): (Sender<Box<dyn TsumugiFuture+ Send>>, Receiver<Box<dyn TsumugiFuture+ Send>>) = mpsc::channel();
-        let (txsendbox, rxsendbox): (Sender<Box<dyn TsumugiTypeChacher+ Send>>, Receiver<Box<dyn TsumugiTypeChacher+ Send>>) = mpsc::channel();
-        let mut tumugi_receipt_list:Vec<Box<dyn TsumugiFuture+ Send>> = Vec::new();
-        let mut pickup_list: Vec<Box<dyn TsumugiTypeChacher+ Send>> =  Vec::new();
-        let mut tsumugi_connect_list: Vec<String> =  Vec::new();
-        let mut tsumugi_object_list: Vec<Box<dyn TsumugiObject+ Send>> =  Vec::new();
-        return Box::from(TsumugiController {
-            pickup_list: pickup_list,
-            pickup_channnel_receiver: rxsendbox,
-            pickup_channnel_sender: txsendbox,
-            receipt_list: tumugi_receipt_list,
-            receipt_channnel_receiver: rxreceivebox,
-            receipt_channnel_sender: txreceivebox,
-            global_tsumugi_controller: self.global_tsumugi_controller,
-            connect_tsumugi_controller: tsumugi_connect_list,
-            global_connect_tsumugi_controller: vec![],
-            tsumugi_controller_name: tsumuginame,
-            tsumugi_object_vector: tsumugi_object_list
+            tsumugi_object_vector: tsumugi_object_list,
         });
+        tc.global_tsumugi_controller = Some(&tc);
+        tc.execute_tsumugi_thread(rxreceivebox, rxsendbox);
+        return tc;
     }
 
-    fn set_object(&mut self, mut tsumugi_object_list: Vec<Box<dyn TsumugiObject+ Send>>) {
+    fn spown(&self, tsumuginame: String) -> Pin<Box<TsumugiController>> {
+        let mut tc = Self::new(tsumuginame);
+        tc.global_tsumugi_controller = self.global_tsumugi_controller;
+        return tc;
+    }
+
+    fn set_object(&mut self, mut tsumugi_object_list: Vec<Box<dyn TsumugiObject + Send>>) {
         self.tsumugi_object_vector.append(&mut tsumugi_object_list);
     }
 
-    fn run(& mut self, tsumugi_function:  &dyn Fn(& TsumugiController) -> Box<TsumugiController>) {
+    fn execute_tsumugi_functions(&mut self, tsumugi_function: &dyn Fn(&TsumugiController) -> Box<TsumugiController>) {
         let mut tc_new = tsumugi_function(self);
         self.global_connect_tsumugi_controller.push(tc_new);
     }
+    fn execute_tsumugi_thread(&self, receipt_channnel_receiver: Receiver<Box<dyn TsumugiFuture + Send>>, pickup_channnel_receiver: Receiver<Box<dyn TsumugiTypeChacher + Send>>) -> JoinHandle<()> {
+        thread::spawn(move || {
+            let mut receiveList: Iter<Box<dyn TsumugiFuture + Send>> = receipt_channnel_receiver.iter();
+            let mut tumugi_receipt_list: Vec<Box<dyn TsumugiFuture + Send>> = Vec::new();
+            let mut pickup_list: Vec<Box<dyn TsumugiTypeChacher + Send>> = Vec::new();
+            if let Some(mut receiveItem) = receiveList.next() {
+                loop {
+                    let mut sendList: Vec<_> = pickup_channnel_receiver.try_iter().collect();
+                    while let Some(sendItem) = sendList.pop() {
+                        if receiveItem.typehash() == sendItem.typehash() {
+                            receiveItem.input_item(sendItem);
+                        }
+                    }
+                }
+            }
+        }
+        )
+    }
 }
-pub fn spown_object_controller<'a>(tc:&'a TsumugiController) -> Box<TsumugiController> {
+
+pub fn spown_object_controller<'a>(tc: &'a TsumugiController) -> Pin<Box<TsumugiController>> {
     let mut newtc = tc.spown("tsumugiobject".to_string());
     newtc.set_object(Vec::new());
-    return newtc
+    return newtc;
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::sync::mpsc::{Sender, Receiver};
 use std::any::{Any, TypeId};
 use std::thread;
 use std::ops::BitAnd;
+use tsumugiEngine::{TsumugiController, TsumugiControllerTrait, TsumugiFuture, TsumugiTypeChacher};
 
 struct ObjectA{
     input_item:Arc<Mutex<i32>>
@@ -34,13 +35,9 @@ struct TsumugiTicket{
     baggettype:TypeId,
     baggatlifetime:BaggetLifeTime
 }
-trait TsumugiFuture:TsumugiTypeChacher{
-    fn poll(self: &mut Self) -> Poll<()>;
-    fn inputItem(self: &mut Self,inputItem: Box<dyn TsumugiTypeChacher+ Send>);
-}
 impl TsumugiFuture for ObjectATicket {
     fn poll(self: &mut Self) -> Poll<()> {
-        if let Some(movaditem) = &self.item {
+        if let Some(movaditem) = self.item.take() {
             let mut rec_obj = self.receive_object.lock().unwrap();
              *rec_obj += movaditem.package;
             dbg!(*rec_obj);
@@ -50,13 +47,21 @@ impl TsumugiFuture for ObjectATicket {
         }
     }
 
-    fn inputItem(self: &mut Self, mut inputItem: Box<dyn TsumugiTypeChacher+ Send>) {
+    fn input_item(&mut self, mut inputItem: Box<dyn TsumugiTypeChacher+ Send>) {
         let movaditem = inputItem.as_any().downcast_mut::<Backet>().unwrap();
         dbg!((*movaditem).package);
-        let receive_item = unsafe {
+        let mut receive_item = unsafe {
+            //ここでメモリリークするぞc0000374
             Box::from_raw(movaditem)
         };
-        self.item = Option::from(receive_item);
+        let boxitem = receive_item.clone();
+        let optionitem = Option::from(boxitem);
+        let receive_item = receive_item as Box<dyn TsumugiTypeChacher+ Send>;
+        //この時点では、inputItemとreceive_itemは同じメモリアドレスの値となっている。
+        std::mem::forget(inputItem);
+        //片方をforgetしてあげないとinputItemとreceive_item両方でメモリ解放が行われてしまう。
+        drop(receive_item);
+        self.item = optionitem;
         self.poll();
     }
 }
@@ -68,13 +73,9 @@ impl TsumugiTypeChacher for ObjectATicket {
         self
     }
 }
-
+#[derive(Clone)]
 struct Backet{
     package:i32
-}
-trait TsumugiTypeChacher{
-    fn as_any(&mut self) -> &mut dyn Any;
-    fn typehash(&self)->TypeId;
 }
 impl TsumugiTypeChacher for Backet{
     fn typehash(&self) -> TypeId {
@@ -84,45 +85,34 @@ impl TsumugiTypeChacher for Backet{
         self
     }
 }
+#[derive(Clone)]
+struct Backet2{
+    package:i32
+}
+impl TsumugiTypeChacher for Backet2{
+    fn typehash(&self) -> TypeId {
+        self.type_id()
+    }
+    fn as_any(&mut self) -> &mut dyn Any {
+        self
+    }
+}
 fn main() {
+    let tsumugiroot = TsumugiController::new("Tsumugi".to_string());
+
     let spown_object_a = ObjectA{ input_item: Arc::new(Mutex::new(50))};
     let mut receive_ticket = spown_object_a.spownticket();
-    let mut tumugiSetList:Vec<Box<dyn TsumugiFuture>> = Vec::new();
-    let (mut txreceivebox, mut rxreceivebox): (Sender<Box<dyn TsumugiFuture+ Send>>, Receiver<Box<dyn TsumugiFuture+ Send>>) = mpsc::channel();
-    txreceivebox.send(Box::new(receive_ticket));
-    let (txsendbox, rxsendbox): (Sender<Box<dyn TsumugiTypeChacher+ Send>>, Receiver<Box<dyn TsumugiTypeChacher+ Send>>) = mpsc::channel();
-    txsendbox.send(Box::new(Backet { package: 12 }));
-    txsendbox.send(Box::new(Backet { package: 20 }));
-    let handle = thread::spawn(move ||{
-
-        let mut receiveList: Vec<_> =  rxreceivebox.try_iter().collect();
-        let mut receiveItem: Box<dyn TsumugiFuture + Send> = receiveList.pop().unwrap();
-        loop{
-            let mut sendList: Vec<_> =  rxsendbox.try_iter().collect();
-            while let Some(sendItem) = sendList.pop(){
-                if receiveItem.typehash() == sendItem.typehash(){
-                    receiveItem.inputItem(sendItem)
-                }
-            }
-        }
-        let mut sendList: Vec<_> =  rxsendbox.try_iter().collect();
-        while let Some(sendItem) = sendList.pop(){
-            if receiveItem.typehash() == sendItem.typehash(){
-                receiveItem.inputItem(sendItem)
-            }
-        }
-        println!("endthread!");
-
-    });
+    tsumugiroot.receipt_channnel_sender.send(Box::new(receive_ticket));
+    tsumugiroot.pickup_channnel_sender.send(Box::new(Backet { package: 12 }));
     loop{
         let mut word = String::new();
         std::io::stdin().read_line(&mut word).ok();
         let answer = word.trim().to_string();
         if answer =="end"{
             break;
-
         }
-        txsendbox.send(Box::new(Backet { package: 20 }));
+        tsumugiroot.pickup_channnel_sender.send(Box::new(Backet { package: 20 }));
+        tsumugiroot.pickup_channnel_sender.send(Box::new(Backet2 { package: 20 }));
     }
     println!("Hello, world!");
 }
